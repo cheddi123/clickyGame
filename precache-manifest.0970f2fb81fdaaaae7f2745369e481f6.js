3c60de98b6484340cad9464e8a0a66f5f28cb18f
Cheddi Charles <cheddi.charles@gmail.com>
2019-01-19 09:21:56 -0600

    Updates

---
+++ precache-manifest.0970f2fb81fdaaaae7f2745369e481f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "5b61a7e470271a120548",
+    "url": "/my-app/static/js/main.5b61a7e4.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/my-app/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "5b61a7e470271a120548",
+    "url": "/my-app/static/css/main.e8d6e752.chunk.css"
+  },
+  {
+    "revision": "86653019a9c9f3cd46ccb0b65381a542",
+    "url": "/my-app/index.html"
+  }
+];
